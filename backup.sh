74a15fac655e94a271ee94ae43ce285a62fd8726
Geraldine Jayo <geraldinejayo@LAPTOP-FMNAVKSG.station>
2023-09-06 19:35:02 +0200

    shell script

---
+++ backup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# This checks if the number of arguments is correct
+# If the number of arguments is incorrect ( $# != 2) print error message and exit
+if [[ $# != 2 ]]
+then
+  echo "backup.sh target_directory_name destination_directory_name"
+  exit
+fi
+
+# This checks if argument 1 and argument 2 are valid directory paths
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]
+then
+  echo "Invalid directory path provided"
+  exit
+fi
+
+targetDirectory=$1
+destinationDirectory=$2
+
+echo "Target Directory: $targetDirectory"
+echo "Destination Directory: $destinationDirectory"
+
+currentTS=`date +%s`
+
+backupFileName="backup-$currentTS.tar.gz"
+
+# We're going to:
+  # 1: Go into the target directory
+  # 2: Create the backup file
+  # 3: Move the backup file to the destination directory
+
+origAbsPath=`pwd`
+
+cd $destinationDirectory
+destDirAbsPath=`pwd`
+
+cd $origAbsPath
+cd $targetDirectory
+
+yesterdayTS=$(($currentTS - 24 * 60 * 60))
+
+declare -a toBackup
+
+for file in $(ls) # [TASK 9]
+do
+  last_mod=`date -r $file +%s`
+  if (( $last_mod > $yesterdayTS ))
+  then
+    toBackup+=($file)
+  fi
+done
+
+tar -czvf $backupFileName ${toBackup[@]}
+
+mv $backupFileName $destDirAbsPath
+
